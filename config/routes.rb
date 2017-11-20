16c69eeb19eda1ce13e13706e3a910908f6e16b2
Elena Maroto <elena_maroto@hotmail.com>
2017-11-20 12:03:59 +0100

    some logic reference

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'restaurants', to: "restaurants#index"
+
+  root to: "pages#home"
+  # get 'home', to: "pages#home"
+
+  get 'about', to: "pages#about"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
